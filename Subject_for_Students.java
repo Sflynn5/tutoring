5ee9b7165d1c1ac1e2c003f956c80d886c125610
Laura Pavlosky <42779779+laurapavlosky@users.noreply.github.com>
2020-02-20 20:24:52 +0000

    Add files via upload

---
+++ Subject_for_Students.java
@@ -0,0 +1,14 @@
+package com.example.tutorfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Subject_for_Students extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject_for__students);
+    }
+}
